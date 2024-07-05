24e0479e3faddbb23d9c45beff1e0666d833d9b8
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-07-06 00:01:29 +0100

    Add Header component with navigation links and CSS styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
 export default function Header() {
   return (
     <header>
       <h1>Blog</h1>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <div className="button">Home</div>
+        </Link>
 
-        <Link to="/posts">Posts</Link>
+        <Link to="/posts">
+          <div className="button">Posts</div>
+        </Link>
 
-        <Link to="/form">Form</Link>
+        <Link to="/form">
+          <div className="button">Add new post</div>
+        </Link>
       </nav>
     </header>
   );
